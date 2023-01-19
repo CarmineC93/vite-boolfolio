0752b120375131f3175c3ef25f928dc8034d06e2
CarmineC93 <107880239+CarmineC93@users.noreply.github.com>
2023-01-19 17:03:14 +0100

    imported axios in app.vue and create a axios caller data from database

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script>
+import axios from 'axios';
+
 export default{
     name: 'App',
     data(){
         return{
-
+            projects:[],
         }
     },
     components:{
@@ -16,6 +18,8 @@ export default{
         getProjects(){
             axios.get('http://127.0.0.1:8000/api/projects').then(resp => {
                 console.log(resp);
+                                // =risposta dell'axios/object nella risposta/nome che abbiamo dato a una delle key dell'object con i dati del database
+                this.projects = resp.data.results
             })
         }
 
