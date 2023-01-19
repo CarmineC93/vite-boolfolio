593b16ccc291e8acd025be5a14dae26f2010ab44
CarmineC93 <107880239+CarmineC93@users.noreply.github.com>
2023-01-19 16:39:28 +0100

    create styles/general.scss, deleted style.css

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script>
+export default{
+    name: 'App',
+    data(){
+        return{
+
+        }
+    },
+    components:{
+
+    },
+    methods:{
+
+    }
+}
+</script>
+
+<template>
+
+</template>
+
+<style lang="scss">
+
+@use "./styles/general.scss" as *;
+
+</style>
