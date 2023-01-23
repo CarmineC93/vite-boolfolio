11e11defcd887b8497f925d3cff4c1140212e507
CarmineC93 <107880239+CarmineC93@users.noreply.github.com>
2023-01-23 17:49:43 +0100

    add type to SingleProject

--- src/pages/SingleProject.vue
+++ src/pages/SingleProject.vue
@@ -11,16 +11,25 @@ export default {
     },
     created() {
         const slug = this.$route.params.slug;
+        console.log('dentro created')
+ 
         axios.get(`${this.store.url}/api/projects/${slug}`).then(resp => {
+
+            console.log('dentro axios then')
             if (resp.data.success) {
                 // Se trovo project
-                this.project = resp.data.post;
+                this.project = resp.data.project;
             } else {
                 // se non trovo project reindirizzo alla pagina not found
                 this.$router.push({ name: "not-found" });
             }
         });
     },
+    computed: {
+        category() {
+            return this.project.type ? this.project.type.name : 'Nessun type';
+        }
+    }
 
 }
 </script>
@@ -28,8 +37,10 @@ export default {
 <template>
     <main>
         <div class="container">
-            <h1 class="mt-3 text-center">{{ project.name }}</h1>
-            <img v-if="project.cover_image" :src="`${store.url}/storage/${project.cover_image}`" alt="">
+            <h1 class="mt-3 text-center">{{ project.title }}</h1>
+            <h5 class="text-center mt-2 text-uppercase text-primary">{{ category }}</h5>
+
+            <img v-if="project.cover_image" :src="`${store.url}/storage/${project.cover_image}`" alt="" style="max-width:300px">
             <div v-else class="text-center mt-4">Nessuna immagine</div>
 
             <p>{{ project.description }}</p>
